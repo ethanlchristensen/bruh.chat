968e9ec8ada4a18478ebd562aa86297e7d32c54b
ethanlchristensen <ethanlchristensen@outlook.com>
2025-11-19 22:11:11 -0600

    [update] - some frontend stuff

---
+++ bruh-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl bg-primary text-primary-foreground">
+        Hello World!
+      </div>
+    </>
+  )
+}
+
+export default App
